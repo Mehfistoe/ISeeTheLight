f347cd02a2c068b916456650d04cffd4d9c47a58
mehfistoe <mckroc468@outlook.com>
2018-01-15 03:22:11 -0500

    Made a better table scraper, added the corrected csv file

--- last_words.py
+++ last_words.py
@@ -1,24 +1,42 @@
-#!/bin/env python
+#!/usr/bin/python3
 
 import csv
-import pandas as pd
-from lxml import html
+#import pandas as pd
+from bs4 import BeautifulSoup
 import requests
-import json
-from watson_developer_cloud import AlchemyLanguageV1
+#import json
 
-web_address = 'http://www.tdcj.state.tx.us/death_row/'
+URL = 'http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html'
 
-dataframe = pd.read_csv('~/TX_Inmate_data.csv')
+def scrape_table():
+    # scraping the table from the website
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    with open('TX_Inmate_data.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
 
-with open('words.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    for index, row in dataframe.iterrows():
-        last_words_page = dataframe.iloc[index, 2]
-        page = requests.get(web_address + last_words_page)
-        tree = html.fromstring(page.content)
-        # last node: //*[@id="body"]/comment()[2]
-        # first node: //*[@id="body"]/p[6]
-        last_words = tree.xpath('//*[@id="body"]/p[position()>5]/text()')
-        # print(last_words)
-        writer.writerow(last_words)
+        # get the content for the header row from the 'th' tags
+        header_row_html = soup.find_all('th')
+        header_row = []
+        for header in header_row_html:
+            header_row.append(header.text)
+        writer.writerow(header_row)
+
+        # get the content for the rows with the 'td' tags
+        rows_html = soup.find_all('tr')
+        for tr in rows_html:
+            row_content = []
+            td = tr.find_all('td')
+            for i in td:
+                if i.a is not None:
+                    row_content.append(i.a.get('href'))
+                else:
+                    row_content.append(i.text)
+            writer.writerow(row_content)
+
+def main():
+    scrape_table()
+
+if __name__ == "__main__":
+    main()
+
