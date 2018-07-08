abe577784ae4fc501977a539998c34686948063e
mehfistoe <mckroc468@outlook.com>
2018-07-08 12:07:20 -0400

    Pulled the last statements form the website and cleaned the HTML tags from them

--- last_words.py
+++ last_words.py
@@ -9,6 +9,19 @@ import numpy as np
 #import json
 
 URL = 'http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html'
+BASE_URL = 'http://www.tdcj.state.tx.us/death_row/'
+inmate_df = pd.read_csv('TX_Inmate_data.csv')
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 def scrape_table():
     # scraping the table from the website
@@ -38,48 +51,57 @@ def scrape_table():
     return
 
 def get_last_statements():
-    BASE_URL = 'http://www.tdcj.state.tx.us/death_row/'
-    inmate_df = pd.read_csv('TX_Inmate_data.csv')
-    # TODO: use BeautifulSoup to got to the webpage with the final statement and scrape it off
-    # TODO: handle TypeErrors for missing entries
     last_statements = [] # column to add to the dataframe
-    paragraphs = [] # STACK to hold each paragraph in the Last Statements page
         
     for i, row in inmate_df.iterrows():
         # Go to the page with the Last Statement on it
         page = requests.get(BASE_URL + row['Last Statement'])
         soup = BeautifulSoup(page.content, "html.parser")
+        paragraphs = [] # to hold each paragraph in the Last Statements page
 
         # locates the phrase "Last Statement" to start scanning from
         key_phrase = re.compile(r'Last Statement')
+        
         try:
             # Loop to find eveything to follow the "Last Statement" key phrase
             for sibling in soup.find('p', text=key_phrase).next_siblings:
-                #print(sibling)
+                # print(sibling)
                 paragraphs.append(str(sibling))
         except TypeError as e:
-            print(e)
+            print("Awkward Entry?")
             paragraphs.append("Awkward Entry?")
-            continue
-        except NameError as n:
-            print(n)
-            paragraphs.append("Awkward Entry?")
-            continue
-        except AttributeError as a:
-            print(a)
-            paragraphs.append("Fuck off")
-            continue
             
-        last_statement_entry = "\n".join(paragraphs)
-        np.append(last_statements, last_statement_entry)
+        except NameError as n:
+            print("Awkward Entry?")
+            paragraphs.append("Awkward Entry?")
+            
+        except AttributeError as a:
+            print("Awkward Entry?")
+            paragraphs.append("Awkward Entry?")
+            
+            
+        if "Awkward Entry?" in paragraphs:
+            last_statement_entry = "Awkward Entry?"
+        else:
+            last_statement_entry = "\n".join(paragraphs)
+            last_statement_entry = BeautifulSoup(last_statement_entry, "lxml").text
+            last_statement_entry = last_statement_entry.replace("InstanceEndEditable", "")
+        
+        print(bcolors.WARNING)
+        print(last_statement_entry)
+        print(bcolors.ENDC)
+        last_statements.append(last_statement_entry)
+        print('Entry added.')
 
-    return last_statements
+    return np.array(last_statements)
 
 
 
 def main():
     # scrape_table()
-    print(get_last_statements())
+    
+    # inmate_df['Last Statements Text'] = get_last_statements()
+    # inmate_df.to_csv('new_inmate.csv')
 
 if __name__ == "__main__":
     main()
